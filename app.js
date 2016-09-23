332e83a428979378729f8af13cf31c8e6b4a0002
Keposmile <726623477@qq.com>
2016-09-23 12:17:27 +0800

    开始做页面样式

--- app.js
+++ app.js
@@ -5,32 +5,40 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
 var app = express();
-
 // all environments
+//映射端口
 app.set('port', process.env.PORT || 3000);
+//配置模板引擎为ejs
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+//配置页面图标
 app.use(express.favicon());
+//终端输出简单日志
 app.use(express.logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+
 app.use(app.router);
+//设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+routes(app);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
+module.exports = function(app){
+  app.get("/",function(req,res){
+    res.render("home");
 
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    console.log("Loading...");
+  });
+  app.get("/blog",function(req,res){
+    res.send("blog page");
+  });
+  app.get("/album",function(req,res){
+    res.send("album page");
+  });
 };
